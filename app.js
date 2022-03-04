c3c0e7d7a3d056d98b583fbaf019783cb7baa86f
shaykhmirzaban <shaykhmirzaban@gmail.com>
2022-03-05 03:59:03 +0500

    assignment 13 completed

---
+++ app.js
@@ -0,0 +1,20 @@
+let long = document.querySelector(".long");
+let long1 = document.querySelector(".long1");
+long.addEventListener("click",f1)
+function f1(){
+    long1.classList.toggle("active");
+    console.log("welcome")
+}
+
+let asd1 = document.querySelector(".asd1");
+let asd2 = document.querySelector(".asd2");
+asd1.addEventListener("click",f2)
+function f2(){
+    asd2.classList.toggle("active");
+    console.log("welcome")
+}
+console.log("welcoem")
+
+
+
+
